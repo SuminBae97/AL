62acca4d48cc00791fc809228d2256e4e643a0bb
SuminBae97 <steavee@o.cnu.ac.kr>
2021-08-10 18:54:56 +0900

    fixed

--- Travel_path.py
+++ Travel_path.py
@@ -1,71 +1,22 @@
 
 
 
-# #tickets = [["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]
-# tickets = [["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]
-#
-# graph={}
-# places=set()
-# visited={}
-# answer=[]
-#
-# for val in tickets:
-#     start = val[0]
-#     arrival = val[1]
-#     places.add(start)
-#     places.add(arrival)
-#     #graph.append({start:[arrival]})
-#     if start in graph.keys():
-#         graph[start].append(arrival)
-#     else:
-#         graph[start] = [arrival]
-# for val in places:
-#     visited[val]=False
-#
-# for val in graph.values():
-#     val.sort()
 
-def dfs(start,visited,answer,graph):
-    visited[start]=True
-    #print(start)
-    answer.append(start)
-    for i in graph[start]:
-        try:
-            if not visited[i] or len(graph[i]) != 2:
-                graph[start].pop(0)
-                dfs(i,visited,answer,graph)
-        except KeyError:
-            return
-
-
-#dfs("ICN")
-#print(answer)
+from collections import defaultdict
 
 
 def solution(tickets):
-    graph = {}
-    places = set()
-    visited = {}
     answer = []
 
-    for val in tickets:
-        start = val[0]
-        arrival = val[1]
-        places.add(start)
-        places.add(arrival)
-        # graph.append({start:[arrival]})
-        if start in graph.keys():
-            graph[start].append(arrival)
-        else:
-            graph[start] = [arrival]
-    for val in places:
-        visited[val] = False
+    graph = defaultdict(list)
+    for dept, arrv in sorted(tickets):
+        graph[dept].append(arrv)
+    def dfs(dept):
+        while graph[dept]:
+            dfs(graph[dept].pop(0))
 
-    for val in graph.values():
-        val.sort()
+        answer.append(dept)
 
-    dfs("ICN",visited,answer,graph)
-    return answer
+    dfs("ICN")
+    return answer[::-1]
 
-#print(solution([["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]))
-#print(solution(tickets = [["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]))
