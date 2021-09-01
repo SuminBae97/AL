233e76a59a66b75383286fba99acab0a87149dfd
SuminBae97 <steavee@o.cnu.ac.kr>
2021-09-01 16:20:05 +0900

    code fix

--- stock_price.py
+++ stock_price.py
@@ -3,13 +3,11 @@ def solution(prices):
     stock_len = len(prices)
 
     for i in range(stock_len):
-        count=0
         for j in range(i+1,stock_len):
             if prices[i] <=prices[j]:
-                count+=1
-        answer[i]=count
+                answer[i]+=1
+            else:
+                answer[i]+=1
+                break
 
     return answer
-
-
-#print(solution([1, 2, 3, 2, 3]))
